4799226127036a3f4e9568a2658fbf3ca5a57b91
polyakale <polyakalex52@gmail.com>
2024-10-01 09:41:27 +0200

    FINE

---
+++ src/components/studyGroupCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h1 class="card-title" v-for="(studyGroup,i) in studyGroups" :key="i">{{studyGroup.name}}</h1>
+      <span v-for="(student, i) in students" :key="i" :value="student.studyGroupId">{{ student.name }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["studyGroups", "students"],
+  computed: {},
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/studyGroups.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="row text-center">
+    <div class="col-6">
+      <table class="my-table">
+        <thead>
+          <th>Student</th>
+          <th>Class</th>
+          <th>Study group</th>
+        </thead>
+        <tbody>
+          <tr v-for="(student, i) in students" :key="i">
+            <td scope="col">{{ student.name }}</td>
+            <td scope="col">{{ student.class }}</td>
+            <td scope="col">
+              <select>
+                <option
+                  v-for="(studyGroup, i) in studyGroups"
+                  :key="i"
+                  value=""
+                >
+                  {{ studyGroup.name }}
+                </option>
+              </select>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="col-6">
+      <studyGroupCard
+      
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import studyGroupCard from "@/components/studyGroupCard.vue";
+export default {
+  data() {
+    return {
+      students: [
+        { id: 1, name: "Tom Stephen", class: "3a", studyGroupId: 1 },
+        { id: 2, name: "Sara Johnson", class: "1b", studyGroupId: 1 },
+        { id: 3, name: "Mike Brown", class: "5c", studyGroupId: 1 },
+        { id: 4, name: "Lisa White", class: "2d", studyGroupId: 1 },
+        { id: 5, name: "James Smith", class: "7a", studyGroupId: 1 },
+        { id: 6, name: "Emma Davis", class: "12b", studyGroupId: 1 },
+        { id: 7, name: "David Wilson", class: "6c", studyGroupId: 1 },
+        { id: 8, name: "Olivia Taylor", class: "11d", studyGroupId: 1 },
+        { id: 9, name: "Sophia Martinez", class: "4a", studyGroupId: 1 },
+        { id: 10, name: "Daniel Anderson", class: "9b", studyGroupId: 1 },
+        { id: 11, name: "Emily Thomas", class: "1c", studyGroupId: 1 },
+        { id: 12, name: "Jacob Jackson", class: "3d", studyGroupId: 1 },
+        { id: 13, name: "Ava White", class: "8a", studyGroupId: 1 },
+        { id: 14, name: "Isabella Harris", class: "2b", studyGroupId: 1 },
+        { id: 15, name: "Mason Clark", class: "10c", studyGroupId: 1 },
+        { id: 16, name: "Liam Lewis", class: "4d", studyGroupId: 1 },
+        { id: 17, name: "Charlotte Lee", class: "7b", studyGroupId: 1 },
+        { id: 18, name: "Noah Hall", class: "5a", studyGroupId: 1 },
+        { id: 19, name: "Lucas Allen", class: "12c", studyGroupId: 1 },
+        { id: 20, name: "Mia Young", class: "9d", studyGroupId: 1 },
+        { id: 21, name: "Ella King", class: "10a", studyGroupId: 1 },
+        { id: 22, name: "Aiden Wright", class: "11b", studyGroupId: 1 },
+        { id: 23, name: "Sofia Scott", class: "6c", studyGroupId: 1 },
+        { id: 24, name: "Jack Green", class: "3a", studyGroupId: 1 },
+        { id: 25, name: "Grace Adams", class: "4b", studyGroupId: 1 },
+      ],
+      studyGroups: [
+        {
+          id: 1,
+          name: "No study group",
+        },
+        {
+          id: 2,
+          name: "Singing",
+        },
+        {
+          id: 3,
+          name: "Data Science",
+        },
+        {
+          id: 4,
+          name: "Soccer",
+        },
+        {
+          id: 5,
+          name: "Handball",
+        },
+      ],
+    };
+  },
+  components: {
+    studyGroupCard,
+  },
+};
+</script>
+
+<style>
+</style>
