d00417df31d587667db65a0e4f985d8c73eccff1
polyakale <polyakalex52@gmail.com>
2024-10-02 14:00:31 +0200

    css progress 33%

--- src/components/studyGroupCard.vue
+++ src/components/studyGroupCard.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="card text-bg-dark">
-    <div class="card-header">
+  <div v-if="StudyGroupStudent.length > 0" class="card">
+    <div class="card-header bg-transparent">
       <h3 class="card-title">{{ studyGroup.name }}</h3>
     </div>
     <div class="card-body">
@@ -9,19 +9,18 @@
         :key="i"
         class="student-item"
       >
-        {{ student.name }}
+        {{ student.name }}<span v-if="i < StudyGroupStudent.length - 1">,</span>
       </span>
     </div>
   </div>
 </template>
-
 
 <script>
 export default {
   props: ["studyGroup", "students"],
   computed: {
     StudyGroupStudent() {
-      return this.students.filter((g) => g.studyGroupId == this.studyGroup.id);
+      return this.students.filter((g) => g.studyGroupId === this.studyGroup.id);
     },
   },
 };
--- src/views/studyGroups.vue
+++ src/views/studyGroups.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row text-center text-color">
-    <div class="col-6 my-table">
+    <div class="col-md-6 my-table">
       <table>
         <thead>
           <tr>
@@ -31,7 +31,7 @@
       </table>
     </div>
 
-    <div class="col-6">
+    <div class="col-md-6">
       <studyGroupCard
         v-for="studyGroup in studyGroups"
         :key="studyGroup.id"
@@ -48,53 +48,33 @@ export default {
   data() {
     return {
       students: [
-        { id: 1, name: "Tom Stephen", class: "3a", studyGroupId: 1 },
-        { id: 2, name: "Sara Johnson", class: "1b", studyGroupId: 3 },
-        { id: 3, name: "Mike Brown", class: "5c", studyGroupId: 5 },
-        { id: 4, name: "Lisa White", class: "2d", studyGroupId: 3 },
-        { id: 5, name: "James Smith", class: "7a", studyGroupId: 4 },
-        { id: 6, name: "Emma Davis", class: "12b", studyGroupId: 1 },
-        { id: 7, name: "David Wilson", class: "6c", studyGroupId: 2 },
-        { id: 8, name: "Olivia Taylor", class: "11d", studyGroupId: 2 },
-        { id: 9, name: "Sophia Martinez", class: "4a", studyGroupId: 5 },
-        { id: 10, name: "Daniel Anderson", class: "9b", studyGroupId: 3 },
-        { id: 11, name: "Emily Thomas", class: "1c", studyGroupId: 5 },
-        { id: 12, name: "Jacob Jackson", class: "3d", studyGroupId: 3 },
-        { id: 13, name: "Ava White", class: "8a", studyGroupId: 2 },
-        { id: 14, name: "Isabella Harris", class: "2b", studyGroupId: 4 },
-        { id: 15, name: "Mason Clark", class: "10c", studyGroupId: 4 },
-        { id: 16, name: "Liam Lewis", class: "4d", studyGroupId: 5 },
-        { id: 17, name: "Charlotte Lee", class: "7b", studyGroupId: 2 },
-        { id: 18, name: "Noah Hall", class: "5a", studyGroupId: 2 },
-        { id: 19, name: "Lucas Allen", class: "12c", studyGroupId: 2 },
-        { id: 20, name: "Mia Young", class: "9d", studyGroupId: 1 },
-        { id: 21, name: "Ella King", class: "10a", studyGroupId: 5 },
-        { id: 22, name: "Aiden Wright", class: "11b", studyGroupId: 3 },
-        { id: 23, name: "Sofia Scott", class: "6c", studyGroupId: 4 },
-        { id: 24, name: "Jack Green", class: "3a", studyGroupId: 4 },
-        { id: 25, name: "Grace Adams", class: "4b", studyGroupId: 1 },
+      { id: 1, name: "Tom Stephen", class: "12.C", studyGroupId: 1 },
+    { id: 2, name: "Sara Johnson", class: "9.A", studyGroupId: 1 },
+    { id: 3, name: "Mike Brown", class: "10.B", studyGroupId: 1 },
+    { id: 4, name: "Lisa White", class: "11.B", studyGroupId: 1 },
+    { id: 5, name: "James Smith", class: "12.C", studyGroupId: 1 },
+    { id: 6, name: "Emma Davis", class: "13.D", studyGroupId: 1 },
+    { id: 7, name: "David Wilson", class: "10.B", studyGroupId: 1 },
+    { id: 8, name: "Olivia Taylor", class: "11.B", studyGroupId: 1 },
+    { id: 9, name: "Sophia Martinez", class: "9.A", studyGroupId: 1 },
+    { id: 10, name: "Daniel Anderson", class: "12.C", studyGroupId: 1 },
+    { id: 11, name: "Emily Thomas", class: "10.B", studyGroupId: 1 },
+    { id: 12, name: "Jacob Jackson", class: "11.B", studyGroupId: 1 },
+    { id: 13, name: "Ava White", class: "9.A", studyGroupId: 1 },
+    { id: 14, name: "Isabella Harris", class: "13.D", studyGroupId: 1 },
+    { id: 15, name: "Mason Clark", class: "12.C", studyGroupId: 1 },
+    { id: 16, name: "Liam Lewis", class: "10.B", studyGroupId: 1 },
+    { id: 17, name: "Charlotte Lee", class: "9.A", studyGroupId: 1 },
+    { id: 18, name: "Noah Hall", class: "11.B", studyGroupId: 1 },
+    { id: 19, name: "Lucas Allen", class: "13.D", studyGroupId: 1 },
+    { id: 20, name: "Mia Young", class: "12.C", studyGroupId: 1 },
       ],
       studyGroups: [
-        {
-          id: 1,
-          name: "No study group",
-        },
-        {
-          id: 2,
-          name: "Singing",
-        },
-        {
-          id: 3,
-          name: "Data Science",
-        },
-        {
-          id: 4,
-          name: "Soccer",
-        },
-        {
-          id: 5,
-          name: "Handball",
-        },
+        { id: 1, name: "No study group"},
+        { id: 2, name: "Soccer"},
+        { id: 3, name: "Robotics"},
+        { id: 4, name: "Python Programming"},
+        { id: 5, name: "Mobile App Development"},
       ],
     };
   },
